3c6ea1117a329d9da678b1a8ef51e0f79ef00b0a
Luxing Huang <hlx98007@d0z.net>
2015-09-18 16:57:38 -0400

    add iptables change in the script

--- shadowsocks/shadowsocks.sh
+++ shadowsocks/shadowsocks.sh
@@ -12,6 +12,10 @@ SERVER_IP=$(curl -sL ip.luxing.im)
 SERVER_PORT=$1
 PASSWORD=$2
 
+# Yes or no, like -y in yum, if given, iptables will be automatically updated
+# and saved.
+YN=$3
+
 if [ -z $SERVER_IP ]; then
     echo 'you are not able to connect to the internet!'
     exit 2
@@ -42,3 +46,19 @@ cd /etc
 sed -i "s/%SERVER_ADDR%/$SERVER_IP/g" ss.json
 sed -i "s/%PORT_NUMBER%/$SERVER_PORT/g" ss.json
 sed -i "s/%PASSWORD%/$PASSWORD/g" ss.json
+
+if [ $YN != 'y' ]; then
+    read -p "Add port to pass firewall? y/n " YN
+fi
+
+if [ $YN = 'y' ]; then
+    iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport $SERVER_PORT -j ACCEPT
+    read -p "Save iptables configurations? y/n" YN
+    if [ $YN = 'y' ]; then
+        iptables-save > /etc/sysconfig/iptables
+    fi
+else
+    echo 'Done...'
+fi
+
+exit 0
