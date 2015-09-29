ce996c7c716fc8eee2a5ed8668cf0ebd13a974bb
Luxing Huang <luhuang@blackberry.com>
2015-09-29 11:31:50 -0400

    add git pull to skel

--- skel/skel.sh
+++ skel/skel.sh
@@ -5,6 +5,9 @@ ZSH_TEMPLATE_LOCATION=$(cd `dirname $0`;cd ..;cd zsh; pwd)
 if [ `whoami` = "root" ]; then
     if [ ! -d "/etc/skel/.oh-my-zsh" ]; then
         git clone https://github.com/robbyrussell/oh-my-zsh.git /etc/skel/.oh-my-zsh
+    else
+        cd .oh-my-zsh
+        git pull
     fi
     touch /etc/skel/.zshrc.local
     cp $ZSH_TEMPLATE_LOCATION/.zshrc /etc/skel/
