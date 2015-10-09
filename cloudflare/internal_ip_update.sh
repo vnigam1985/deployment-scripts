b5646caa2ddfd8feae21727b19028b4c8b11fe7e
Luxing Huang <luhuang@blackberry.com>
2015-10-09 14:58:54 -0400

    add usage comments on internal ip update

--- cloudflare/internal_ip_update.sh
+++ cloudflare/internal_ip_update.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+# You need to fill out the blanks below:
+# id, email, tkn, z, name
 curl https://www.cloudflare.com/api_json.html -d "a=rec_edit" -d "id=" -d "email=" -d "tkn=" -d "type=A" -d "z=" -d "name=" -d "content=`hostname -I | sed -e "s/ 192.168.122.1//g" | sed -e "s/ //g"`" -d "ttl=1" -d "service_mode=0"
 echo
