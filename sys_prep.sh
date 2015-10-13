54ef95831dffc2a010930cd247ca6f1917d15d1b
Luxing Huang <luhuang@blackberry.com>
2015-10-13 16:24:38 -0400

    switching to curl

--- sys_prep.sh
+++ sys_prep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This script is used for deployment scripting purpose. It executes straightly
 # after the system installation and it is supposely to be run under root.
-# Use this: wget url -O /tmp/prep.sh; sh /tmp/prep.sh
+# Use this: curl https://raw.githubusercontent.com/hlx98007/deployment-scripts/master/sys_prep.sh -o /tmp/prep.sh; sh /tmp/prep.sh
 # only works under root
 if [ `whoami` != 'root' ]; then
     echo "Needs to be root"
