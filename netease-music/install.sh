5fa1aa9b460093c5d1f9401674d39cd8cdab8aa3
Luxing Huang <luhuang@blackberry.com>
2015-12-10 15:08:20 -0500

    adding hosts manip

--- netease-music/install.sh
+++ netease-music/install.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
-sudo yum install python-pip mpg123 python-keybinder -y
-sudo pip install NetEase-MusicBox
+if [ `whoami` != 'root' ]; then
+    echo "needs to be root"
+    exit 1
+fi
+
+# install dependencies
+yum install python-pip mpg123 python-keybinder -y
+pip install NetEase-MusicBox
 
 # comment this out if you are living within China.
-for i in $(seq 1 10)
-do
-    sudo echo "151.80.238.242   m${i}.music.126.net" >> /etc/hosts
-done
+grep '151.80' /etc/hosts > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    for i in $(seq 1 10)
+    do
+        echo "151.80.238.242   m${i}.music.126.net" >> /etc/hosts
+    done
+else
+    echo "skipped hosts operation"
+fi
