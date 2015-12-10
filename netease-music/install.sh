0aaaadabf17e3be6c5179979b0c84085fd844781
Luxing Huang <luhuang@blackberry.com>
2015-12-10 14:58:23 -0500

    adding sudo to hosts management

--- netease-music/install.sh
+++ netease-music/install.sh
@@ -5,5 +5,5 @@ sudo pip install NetEase-MusicBox
 # comment this out if you are living within China.
 for i in $(seq 1 10)
 do
-    echo "151.80.238.242   m${i}.music.126.net" >> /etc/hosts
+    sudo echo "151.80.238.242   m${i}.music.126.net" >> /etc/hosts
 done
