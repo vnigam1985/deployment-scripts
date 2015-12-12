8cba0786ed0fe817df0a6c5487b3eaa0445db60a
Luxing Huang <hlx98007@gmail.com>
2015-12-11 21:21:09 -0500

    add python-crypto to the list

--- netease-music/install.sh
+++ netease-music/install.sh
@@ -11,13 +11,13 @@ release=`lsb_release -r | awk '{print $2}'`
 
 # install RPMFusion for different distro
 if [ $distribution = 'Fedora' ]; then
-    dnf install python-pip python-keybinder http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-${release}.noarch.rpm -y
+    dnf install python-pip python-crypto python-keybinder http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-${release}.noarch.rpm -y
 elif [ $distribution = 'CentOS' ]; then
     yum install epel-release -y
     if [ $release = '7' ]; then
-        yum install python-pip python-keybinder http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm -y
+        yum install python-pip python-crypto python-keybinder http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm -y
     else
-        yum install python-pip python-keybinder http://download1.rpmfusion.org/free/el/updates/$release/i386/rpmfusion-free-release-$release-1.noarch.rpm -y
+        yum install python-pip python-crypto python-keybinder http://download1.rpmfusion.org/free/el/updates/$release/i386/rpmfusion-free-release-$release-1.noarch.rpm -y
     fi
 fi
 # install mpg123
