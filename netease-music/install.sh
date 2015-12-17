9fe1af0c3ad1c68eb5550eba90011fb88d168868
Luxing Huang <luhuang@blackberry.com>
2015-12-17 13:27:35 -0500

    update jump server

--- netease-music/install.sh
+++ netease-music/install.sh
@@ -33,12 +33,12 @@ fi
 # install netease musicbox
 pip2 install NetEase-MusicBox
 # comment this out if you are living within China.
-grep '151.80' /etc/hosts > /dev/null 2>&1
+grep '209.141' /etc/hosts > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
     for i in $(seq 1 10)
     do
-        echo "151.80.238.242   m${i}.music.126.net" >> /etc/hosts
+        echo "209.141.33.14 m${i}.music.126.net" >> /etc/hosts
     done
 else
     echo "skipped hosts operation"
