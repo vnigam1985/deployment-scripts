5390f1d38b98138dd3afea6a4844671b60dbf607
Luxing Huang <hlx98007@gmail.com>
2016-01-07 19:42:58 -0500

    separate config and setup

--- netease-music/install.sh
+++ netease-music/install.sh
@@ -29,17 +29,3 @@ if [ $? -ne 0 ]; then
     echo "something is wrong"
     exit 1
 fi
-
-# install netease musicbox
-pip2 install NetEase-MusicBox
-# comment this out if you are living within China.
-grep '209.141' /etc/hosts > /dev/null 2>&1
-
-if [ $? -ne 0 ]; then
-    for i in $(seq 1 10)
-    do
-        echo "209.141.33.14 m${i}.music.126.net" >> /etc/hosts
-    done
-else
-    echo "skipped hosts operation"
-fi
