e525b2ac5aa8457d12fad375d5c3094dedd72333
Luxing Huang <luhuang@blackberry.com>
2016-01-13 16:32:22 -0500

    add some snippet from other git repository

--- cloudflare/ddns.py
+++ cloudflare/ddns.py
@@ -7,17 +7,24 @@ import requests as rq
 import json
 import sys
 import subprocess
+import socket
+import fcntl
+import struct
 
 API_URL = "https://www.cloudflare.com/api_json.html"
+arg_length = len(sys.argv)
 
 # Get global value
-if len(sys.argv) == 5:
+if arg_length >= 5:
     APPKEY = sys.argv[1]
     EMAIL = sys.argv[2]
     DOMAIN = sys.argv[3]
     SUBDOMAIN = sys.argv[4]
+    if arg_length == 6:
+      dev = sys.argv[5]
 else:
-    print("Please provide appkey, email, domain name and subdomain name")
+    print("%s appkey email domain_name subdomain_name [device]" % sys.argv[0],
+        file=sys.stderr)
     sys.exit(1)
 
 
@@ -62,11 +69,25 @@ def ddns(subdomain_id, wan_ip):
         print("success")
 
 
+def get_my_internal_ip(dev):
+    """
+    Grab from https://github.com/lilydjwg/winterpy/blob/master/pylib/netutils.py
+    """
+    ifname = ifname.encode("ascii")
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip = socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
+        ifname[:15]))[20:24])
+    return ip
+
 if __name__ == '__main__':
     subdomain_id = retrieve_id()
     if subdomain_id is None:
         print("Sub domain not found.", file=sys.stderr)
         sys.exit(2)
-        
-    current_wan_ip = getip()
+
+    if arg_length == 6:
+        current_wan_ip = get_my_internal_ip(dev)
+    else:
+        current_wan_ip = getip()
+
     ddns(subdomain_id, current_wan_ip)
